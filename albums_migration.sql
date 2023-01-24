654f15d2f3f3b9f6ac83bbb85765a76203c4c7ff
Luke Van Delden <vandelden.luke@gmail.com>
2023-01-24 09:19:46 -0600

    final edit for tables

--- albums_migration.sql
+++ albums_migration.sql
@@ -3,11 +3,11 @@ USE codeup_test_db;
 DROP TABLE IF EXISTS albums;
 
 CREATE TABLE albums (
-                        id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+                        id INT UNSIGNED AUTO_INCREMENT,
                         artist VARCHAR(50),
                         name  VARCHAR(100),
-                        release_date  INT NOT NULL,
-                        sales  float NOT NULL,
-                        genre  VARCHAR(50) NOT NULL,
+                        release_date  INT,
+                        sales  float,
+                        genre  VARCHAR(50),
                         PRIMARY KEY (id)
 );
