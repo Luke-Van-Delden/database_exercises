f3133276a3a9f06cf9e0906e6ad1057a52f0b8b6
Luke Van Delden <vandelden.luke@gmail.com>
2023-01-25 13:42:13 -0600

    completed where clauses

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,11 +1,11 @@
 USE codeup_test_db;
 
--- SELECT name AS 'Pink Floyd' FROM albums WHERE artist = 'Pink Floyd';
+SELECT name AS 'Pink Floyd' FROM albums WHERE artist = 'Pink Floyd';
 
--- SELECT release_date AS 'Sgt. Pepper''s Lonely Hearts Club Band Year Released' FROM albums WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
+SELECT release_date AS 'Sgt. Pepper''s Lonely Hearts Club Band Year Released' FROM albums WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
 
--- SELECT genre AS 'Metallica''s Genre' FROM albums WHERE name = 'Metallica';
+SELECT genre AS 'Metallica''s Genre' FROM albums WHERE name = 'Metallica';
 
--- SELECT name AS 'Albums released in 1990s' FROM albums WHERE release_date BETWEEN 1990 AND 1999;
+SELECT name AS 'Albums released in 1990s' FROM albums WHERE release_date BETWEEN 1990 AND 1999;
 
--- SELECT name AS 'Rock Albums' FROM albums WHERE genre = 'Rock';
+SELECT name AS 'Rock Albums' FROM albums WHERE genre = 'Rock';
