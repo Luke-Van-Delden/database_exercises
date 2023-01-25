d6c7cb9ea7d8d8478da97391998b57f1ecaf1d41
Luke Van Delden <vandelden.luke@gmail.com>
2023-01-25 09:39:10 -0600

    polishing

--- update_exercises.sql
+++ update_exercises.sql
@@ -4,10 +4,10 @@ USE codeup_test_db;
 -- UPDATE albums SET sales = sales * 10;
 -- SELECT name AS 'All albums <30M Sales' FROM albums;
 
--- SELECT name AS 'From 1980s' FROM albums WHERE release_date < 1980;
+-- SELECT name AS 'From 1980s', release_date FROM albums WHERE release_date < 1980;
 -- UPDATE albums SET release_date = 1800 WHERE release_date < 1980;
--- SELECT name AS 'From 1980s' FROM albums WHERE release_date < 1980;
+-- SELECT name AS 'From 1980s', release_date FROM albums WHERE release_date < 1980;
 
--- SELECT name AS 'By Michael Jackson' FROM albums WHERE artist = 'Michael Jackson';
+SELECT name AS 'Albums By Michael Jackson', artist FROM albums WHERE artist = 'Michael Jackson';
 -- UPDATE albums SET artist = 'Peter Jackson' WHERE artist = 'Michael Jackson';
--- SELECT name AS 'By Peter Jackson' FROM albums WHERE artist = 'Peter Jackson';
+-- SELECT name AS 'By Peter Jackson', artist FROM albums WHERE artist = 'Peter Jackson';
