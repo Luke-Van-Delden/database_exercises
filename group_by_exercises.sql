b7f7c5833a1e1c8f3828255ef566a94ff5b4455e
Luke Van Delden <vandelden.luke@gmail.com>
2023-01-26 15:21:40 -0600

    group by exercise completed

---
+++ group_by_exercises.sql
@@ -0,0 +1,23 @@
+USE employees;
+
+SELECT title FROM titles GROUP BY title;
+
+SELECT last_name FROM employees
+WHERE last_name LIKE 'E%' AND last_name LIKE '%E'
+GROUP BY last_name ORDER BY last_name;
+
+SELECT first_name, last_name FROM employees
+WHERE last_name LIKE 'E%' AND last_name LIKE '%E'
+GROUP BY first_name, last_name;
+
+SELECT last_name FROM employees
+WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%'
+GROUP BY last_name;
+
+SELECT COUNT(last_name), last_name FROM employees
+WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%'
+GROUP BY last_name;
+
+SELECT COUNT(*), gender FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+GROUP BY gender;
