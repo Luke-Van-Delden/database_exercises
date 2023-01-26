c44d41172e1d2a5d4cd2da39785b94bcbaa48b76
Luke Van Delden <vandelden.luke@gmail.com>
2023-01-26 09:59:01 -0600

    completed limit exercises

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -1,9 +1,11 @@
 USE employees;
 
 
-SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY last_name DESC, first_name DESC;
 
-SELECT * FROM employees WHERE last_name LIKE 'E%';
+SELECT * FROM employees WHERE last_name LIKE 'E%' ORDER BY emp_no;
+SELECT * FROM employees WHERE last_name LIKE 'E%' ORDER BY emp_no DESC;
+
 
 SELECT * FROM employees WHERE last_name LIKE '%q%';
 
@@ -16,4 +18,3 @@ SELECT * FROM employees WHERE last_name LIKE 'E%' OR last_name LIKE '%E';
 SELECT * FROM employees WHERE last_name LIKE 'E%' AND last_name LIKE '%E';
 
 SELECT * FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
-
