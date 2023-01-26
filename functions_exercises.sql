448d3c04f46bf25c30f13272426b0c5919136d61
Luke Van Delden <vandelden.luke@gmail.com>
2023-01-26 14:17:57 -0600

    polish

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -23,8 +23,6 @@ SELECT CONCAT(first_name, ' ', last_name) FROM employees WHERE last_name LIKE 'E
 
 SELECT * FROM employees WHERE month(birth_date) = 12 AND day(birth_date) = 25;
 
-SELECT * FROM employees WHERE (year(hire_date) BETWEEN 1990 AND 1999) AND (month(birth_date) = 12 AND day(birth_date) = 25);
+SELECT * FROM employees WHERE (year(hire_date) BETWEEN 1990 AND 1999) AND (month(birth_date) = 12 AND day(birth_date) = 25) ORDER BY hire_date DESC;
 
-
-
-# release_date BETWEEN 1990 AND 1999
+SELECT first_name, last_name, DATEDIFF(curdate(),hire_date) FROM employees WHERE (year(hire_date) BETWEEN 1990 AND 1999) AND (month(birth_date) = 12 AND day(birth_date) = 25) ORDER BY hire_date DESC;
